a68f2849b9d0d6bbf90d49fc1fabe3232d6bcbc5
gmazzuca <71595211+gmazzuca@users.noreply.github.com>
2026-02-12 17:58:19 +0000

    Deploying to gh-pages from @ gmazzuca/gmazzuca.github.io@1e76f442cf319d3c5fafc3dda8f66c6ccaa69902 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-nls-max-universality",
-        
-          title: "Nls Max Universality",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/NLS-max-universality/";
-          
-        },
-      },{id: "post-the-q-deformed-marchenko-pastur-law",
+        },{id: "post-the-q-deformed-marchenko-pastur-law",
         
           title: "The q-deformed Marchenko-Pastur law",
         
